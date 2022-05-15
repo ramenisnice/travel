192ea46c180f7002b14e586f39052e42ba79c3a0
Yue Xiong <xiongyueee@outlook.com>
2022-05-15 22:38:07 +0800

    add recommendation part

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -2,14 +2,18 @@
   <HomeHeader />
   <HomeSwiper />
   <HomeIcons />
+  <HomeRecom />
+  <HomeWeekend />
 </template>
 
 <script>
 import HomeHeader from "./components/HomeHeader.vue";
 import HomeSwiper from "./components/HomeSwiper.vue";
 import HomeIcons from "./components/HomeIcons.vue";
+import HomeRecom from "./components/HomeRecom.vue";
+import HomeWeekend from "./components/HomeWeekend.vue";
 
 export default {
-  components: { HomeHeader, HomeSwiper, HomeIcons },
+  components: { HomeHeader, HomeSwiper, HomeIcons, HomeRecom, HomeWeekend },
 };
 </script>
