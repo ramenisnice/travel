cf7d3017e043ee05a449290a74d3b786164263a3
Yue Xiong <xiongyueee@outlook.com>
2022-05-16 16:13:20 +0800

    city router

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "./pages/home/HomePage.vue";
+import CityPage from "./pages/city/CityPage.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -7,6 +8,10 @@ const router = createRouter({
     {
       path: "/",
       component: HomePage,
+    },
+    {
+      path: "/city",
+      component: CityPage,
     },
   ],
 });
