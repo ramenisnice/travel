912f7d13667364ad636601d226f6f4f019d338bc
Yue Xiong <xiongyueee@outlook.com>
2022-05-17 00:39:49 +0800

    add search to city

--- src/pages/city/CityPage.vue
+++ src/pages/city/CityPage.vue
@@ -1,10 +1,14 @@
 <template>
-  <CityHeader />
+  <div>
+    <CityHeader />
+    <CitySearch />
+  </div>
 </template>
 
 <script>
 import CityHeader from "./components/CityHeader.vue";
+import CitySearch from "./components/CitySearch.vue";
 export default {
-  components: { CityHeader },
+  components: { CityHeader, CitySearch },
 };
 </script>
